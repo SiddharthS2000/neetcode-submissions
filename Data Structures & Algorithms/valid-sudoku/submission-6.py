35be9b3c9e1d8a5e326883f01c22eee1786fa962
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-08-11 00:28:29 +0530

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        subtables = defaultdict(set)
+
+        for row in range(9):
+            for column in range(9):
+                if board[row][column] == '.':
+                    continue
+                
+                if (
+                    board[row][column] in rows[row] or 
+                    board[row][column] in columns[column] or 
+                    board[row][column] in subtables[(row // 3, column // 3)]
+                ):
+                    return False
+                
+                rows[row].add(board[row][column])
+                columns[column].add(board[row][column])
+                subtables[(row//3, column//3)].add(board[row][column])
+        return True
+        
