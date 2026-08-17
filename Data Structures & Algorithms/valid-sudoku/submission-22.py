1f84db42d9ff0155b477d53ca65bed42dbb6098f
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-08-17 12:49:03 +0530

    Add: valid-sudoku - submission-22

---
+++ Data Structures & Algorithms/valid-sudoku/submission-22.py
@@ -0,0 +1,58 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        column = defaultdict(set)
+        subtable = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if board[r][c] in row[r] or board[r][c] in column[c] or board[r][c] in subtable[(r//3, c//3)]:
+                    return False
+                
+                row[r].add(board[r][c])
+                column[c].add(board[r][c])
+                subtable[(r//3, c//3)].add(board[r][c])
+            
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # row_record = defaultdict(set)
+        # col_record = defaultdict(set)
+        # subtable_record = defaultdict(set)
+
+        # for r in range(9):
+        #     for c in range(9):
+        #         if board[r][c] == ".":
+        #             continue
+        #         if board[r][c] in row_record[r] or board[r][c] in col_record[c] or board[r][c] in subtable_record[(r//3, c//3)]:
+        #             return False
+            
+        #         row_record[r].add(board[r][c])
+        #         col_record[c].add(board[r][c])
+        #         subtable_record[(r//3, c//3)].add(board[r][c])
+
+        # return True
+
+
+
+        
